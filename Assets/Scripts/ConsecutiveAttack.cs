cd205901b541df98b3d5b1c410fc0d7a9442cd1a
carlo-baron <carlobaron430@gmail.com>
2024-10-16 18:26:36 +0800

    Consecutive attack started
    needs finishing, caching atk and resetting after some time. Optimize code as well

---
+++ Assets/Scripts/ConsecutiveAttack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsecutiveAttack : StateMachineBehaviour
+{
+    PrototypeAttack anim;
+
+    void Awake(){
+        anim = GameObject.FindObjectOfType<PrototypeAttack>();
+    }
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        anim.canAttack = true;
+        anim.isAttack = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(Input.GetMouseButtonDown(0) && anim.canAttack){
+            anim.NextAttack();
+            anim.canAttack = false;
+            anim.isAttack = true;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    // override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+
+    // }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
